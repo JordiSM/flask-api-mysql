8671b4d5fac940dfead36424bb6557e488fb93cc
Jordism <jordi.sevilla1@estudiant.uib.cat>
2022-03-29 23:42:37 +0200

    Getting to know flask & jinja2. Added navbar and README

--- flaskpage.py
+++ flaskpage.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+post = [
+    {
+        'author': 'Jordi Sevilla',
+        'title': 'Blog Post 1',
+        'content':'Tonto quien lo lea',
+        'date_posted': '22 Marzo, 2022'
+    }
+    ,
+    {
+        'author': 'Lluis Mayans',
+        'title': 'Blog Post 2',
+        'content':'Second post content',
+        'date_posted': '22 Marzo, 2022'
+    }
+]
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=post, title='Home')
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
